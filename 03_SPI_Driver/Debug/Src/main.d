af86d109e89ed64a082981979c5338d562cc8977
Mounikagit config user.name git config --global user.name Mounika <mouni141188@gmail.com>
2025-10-09 17:35:24 +0530

    gpio

---
+++ 03_SPI_Driver/Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/03/Drivers/Inc/stm32l4_spi_driver.h
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/03/Drivers/Inc/stm32l4_spi_driver.h:
