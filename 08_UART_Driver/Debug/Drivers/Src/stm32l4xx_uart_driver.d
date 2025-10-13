72b457436f504e2132f43e31645be0a42ad1d923
Mounikagit config user.name git config --global user.name Mounika <mouni141188@gmail.com>
2025-10-13 17:45:37 +0530

    uart

---
+++ 08_UART_Driver/Debug/Drivers/Src/stm32l4xx_uart_driver.d
@@ -0,0 +1,6 @@
+Drivers/Src/stm32l4xx_uart_driver.o: \
+ ../Drivers/Src/stm32l4xx_uart_driver.c \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/08_UART_Driver/Drivers/Inc/stm32l4xx_uart_driver.h \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/08_UART_Driver/Drivers/Inc/stm32l4xx.h
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/08_UART_Driver/Drivers/Inc/stm32l4xx_uart_driver.h:
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/08_UART_Driver/Drivers/Inc/stm32l4xx.h:
---
+++ 08_UART_Driver/Debug/Src/main.d
@@ -0,0 +1,7 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/08_UART_Driver/Drivers/Inc/stm32l4xx_uart_driver.h \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/08_UART_Driver/Drivers/Inc/stm32l4xx.h \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/08_UART_Driver/Drivers/Inc/stm32l4xx_gpio_driver.h
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/08_UART_Driver/Drivers/Inc/stm32l4xx_uart_driver.h:
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/08_UART_Driver/Drivers/Inc/stm32l4xx.h:
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/08_UART_Driver/Drivers/Inc/stm32l4xx_gpio_driver.h:
