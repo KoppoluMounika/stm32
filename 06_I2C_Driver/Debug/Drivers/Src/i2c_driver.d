fc68a13ce9bbac2f38bbe64a9b551499790534ca
Mounikagit config user.name git config --global user.name Mounika <mouni141188@gmail.com>
2025-10-13 14:45:29 +0530

    i2c

---
+++ 06_I2C_Driver/Debug/Drivers/Src/i2c_driver.d
@@ -0,0 +1,7 @@
+Drivers/Src/i2c_driver.o: ../Drivers/Src/i2c_driver.c \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/06_I2C_Driver/Drivers/Inc/stm32l4xx_i2c_driver.h \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/06_I2C_Driver/Drivers/Inc/stm32l4xx.h \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/06_I2C_Driver/Drivers/Inc/stm32l4xx.h
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/06_I2C_Driver/Drivers/Inc/stm32l4xx_i2c_driver.h:
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/06_I2C_Driver/Drivers/Inc/stm32l4xx.h:
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/06_I2C_Driver/Drivers/Inc/stm32l4xx.h:
---
+++ 06_I2C_Driver/Debug/Src/main.d
@@ -0,0 +1,9 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/06_I2C_Driver/Drivers/Inc/stm32l4xx_i2c_driver.h \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/06_I2C_Driver/Drivers/Inc/stm32l4xx.h \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/06_I2C_Driver/Drivers/Inc/stm32l4xx_gpio_driver.h \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/06_I2C_Driver/Drivers/Inc/stm32l4xx.h
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/06_I2C_Driver/Drivers/Inc/stm32l4xx_i2c_driver.h:
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/06_I2C_Driver/Drivers/Inc/stm32l4xx.h:
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/06_I2C_Driver/Drivers/Inc/stm32l4xx_gpio_driver.h:
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/06_I2C_Driver/Drivers/Inc/stm32l4xx.h:
