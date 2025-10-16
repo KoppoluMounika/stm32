0cee3217c3cbb9c9a414f8ddf7ca0c0932167ae0
Mounikagit config user.name git config --global user.name Mounika <mouni141188@gmail.com>
2025-10-16 17:17:22 +0530

    lcd

---
+++ 10_LCD/Debug/Drivers/Src/lcd.d
@@ -0,0 +1,7 @@
+Drivers/Src/lcd.o: ../Drivers/Src/lcd.c \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/10_LCD/Drivers/Inc/lcd.h \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/10_LCD/Drivers/Inc/stm32l4xx_gpio_driver.h \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/10_LCD/Drivers/Inc/stm32l4xx.h
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/10_LCD/Drivers/Inc/lcd.h:
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/10_LCD/Drivers/Inc/stm32l4xx_gpio_driver.h:
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/10_LCD/Drivers/Inc/stm32l4xx.h:
---
+++ 10_LCD/Debug/Src/main.d
@@ -0,0 +1,7 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/10_LCD/Drivers/Inc/lcd.h \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/10_LCD/Drivers/Inc/stm32l4xx_gpio_driver.h \
+ C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/10_LCD/Drivers/Inc/stm32l4xx.h
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/10_LCD/Drivers/Inc/lcd.h:
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/10_LCD/Drivers/Inc/stm32l4xx_gpio_driver.h:
+C:/Users/KOPPOLU\ MOUNIKA/OneDrive\ -\ BITSILICA\ Pvt.\ Ltd/Desktop/stm32/10_LCD/Drivers/Inc/stm32l4xx.h:
